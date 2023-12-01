297089523876ebea7265bf103b7cc02aaba4fe9e
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-12-01 17:28:29 +0000

    maven project setup

---
+++ src/main/java/com/mustapha/App.java
@@ -0,0 +1,13 @@
+package com.mustapha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
